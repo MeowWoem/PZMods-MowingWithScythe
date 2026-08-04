000fa198d43f8614cb4ff26975040c01cd2f10e1
Amenophis <l.stoller@outlook.fr>
2026-08-04 23:09:55 +0200

    Move the option under the Gardening submenu

--- Contents/mods/MowingWithScythe/42/media/lua/client/ISUI/MWS_ContextMenu.lua
+++ Contents/mods/MowingWithScythe/42/media/lua/client/ISUI/MWS_ContextMenu.lua
@@ -16,14 +16,37 @@ function ContextMenu.onMowGrass(player, scythe)
 end
 
 function ContextMenu.addMowContextOption(player, context, worldObjects, test)
+	
 	local playerObj = getSpecificPlayer(player)
-	local playerInv = playerObj:getInventory()
-	if playerObj:isAsleep() then return end
-	local scythe = playerInv:getFirstEvalRecurse(predicateScythe);
-	if scythe and not playerObj:getVehicle() then
-		if test == true then return true; end
-		context:addGetUpOption(getText("ContextMenu_MowGrass"), playerObj, ContextMenu.onMowGrass, scythe);
-	end
+    local playerInv = playerObj:getInventory()
+    if playerObj:isAsleep() then return end
+    
+    local scythe = playerInv:getFirstEvalRecurse(predicateScythe)
+    if scythe and not playerObj:getVehicle() then
+        if test == true then return true end
+
+        local gardeningText = getText("ContextMenu_Gardening")
+        local gardeningOption = nil
+        local gardeningSubMenu = nil
+
+        for _, opt in ipairs(context.options) do
+            if opt.name == gardeningText then
+                gardeningOption = opt
+                break
+            end
+        end
+
+        if gardeningOption then
+            gardeningSubMenu = context:getSubMenu(gardeningOption.subOption)
+        else
+            gardeningOption = context:addOption(gardeningText, nil, nil)
+            gardeningSubMenu = ISContextMenu:getNew(context)
+            context:addSubMenu(gardeningOption, gardeningSubMenu)
+        end
+
+        local opt = gardeningSubMenu:addOption(getText("ContextMenu_MowGrass"), playerObj, ContextMenu.onMowGrass, scythe);
+		opt.iconTexture = scythe:getTexture();
+    end
 end
 
 Events.OnFillWorldObjectContextMenu.Add(ContextMenu.addMowContextOption)
