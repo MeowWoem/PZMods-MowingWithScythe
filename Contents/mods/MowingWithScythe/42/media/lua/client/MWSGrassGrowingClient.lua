83c4ab663e8f0f969d506eb98b61433621cb015a
Amenophis <l.stoller@outlook.fr>
2026-08-29 18:03:22 +0200

    Code cleaning

--- Contents/mods/MowingWithScythe/42/media/lua/client/MWSGrassGrowingClient.lua
+++ Contents/mods/MowingWithScythe/42/media/lua/client/MWSGrassGrowingClient.lua
@@ -4,23 +4,23 @@
 
 require "MWSGrassGrowingShared"
 
-MWSGrassGrowingClient = MWSGrassGrowingClient or {}
+MWSGrassGrowingClient = MWSGrassGrowingClient or {};
 
 function MWSGrassGrowingClient.OnServerCommand(module, command, args)
-    if module ~= MWSGrassGrowing.MODULE_NAME then return end
+    if module ~= MWSGrassGrowing.MODULE_NAME then return; end
 
     if command == "confirmPlantSeeds" then
-        local square = getCell():getGridSquare(args.x, args.y, args.z)
+        local square = getCell():getGridSquare(args.x, args.y, args.z);
         if square then
-            MWSGrassGrowing.plantSeedsOnSquare(square)
+            MWSGrassGrowing.plantSeedsOnSquare(square);
         end
 
     elseif command == "denyPlantSeeds" then
-        local player = getSpecificPlayer(0)
+        local player = getSpecificPlayer(0);
         if player then
-            HaloTextHelper.addText(player, "Unable to plant here", HaloTextHelper.getColorRed())
+            HaloTextHelper.addText(player, "Unable to plant here", HaloTextHelper.getColorRed());
         end
     end
 end
 
-Events.OnServerCommand.Add(MWSGrassGrowingClient.OnServerCommand)
+Events.OnServerCommand.Add(MWSGrassGrowingClient.OnServerCommand);
--- Contents/mods/MowingWithScythe/42/media/lua/server/ISMowingCursor.lua
+++ Contents/mods/MowingWithScythe/42/media/lua/server/ISMowingCursor.lua
@@ -11,29 +11,29 @@ local bhc = getCore():getBadHighlitedColor();
 
 
 function ISMowingCursor:create(x, y, z, north, sprite)
-	local playerObj = self.character
+	local playerObj = self.character;
 	local sq = getSquare(x, y, z);
-	ISInventoryPaneContextMenu.equipWeapon(self.scythe, true, true, playerObj:getPlayerNum())
+	ISInventoryPaneContextMenu.equipWeapon(self.scythe, true, true, playerObj:getPlayerNum());
 	self:walkTo(x, y, z);
 	ISTimedActionQueue.add(ISMowing:new(playerObj, self.scythe, sq, self.radius));
 end
 
 function ISMowingCursor:walkTo(x, y, z)
-	local playerObj = self.character
-	local squares = self:getSquares(x, y, z)
-    local closestSq = self:getClosestSquare(squares)
+	local playerObj = self.character;
+	local squares = self:getSquares(x, y, z);
+    local closestSq = self:getClosestSquare(squares);
     if playerObj:getCurrentSquare() == closestSq then
-        return true
+        return true;
     end
-    local adjacent = AdjacentFreeTileFinder.Find(closestSq, self.character)
-    if not adjacent then return false end
-    ISTimedActionQueue.add(ISWalkToTimedAction:new(playerObj, adjacent))
-	return true
+    local adjacent = AdjacentFreeTileFinder.Find(closestSq, self.character);
+    if not adjacent then return false; end
+    ISTimedActionQueue.add(ISWalkToTimedAction:new(playerObj, adjacent));
+	return true;
 end
 
 
 function ISMowingCursor:isValid(square)
-	return self:isValidArea(square:getX(), square:getY(), square:getZ())
+	return self:isValidArea(square:getX(), square:getY(), square:getZ());
 end
 
 function ISMowingCursor:isValidArea(x, y, z, renderMode)
@@ -46,7 +46,7 @@ function ISMowingCursor:isValidArea(x, y, z, renderMode)
 			isCouldSee = true;
 		end
 		for i=sq:getObjects():size(),1,-1 do
-			local o = sq:getObjects():get(i-1)
+			local o = sq:getObjects():get(i-1);
 			if ISMowing.isCuttable(o) then
 				hasGrass = true;
 				if(renderMode) then
@@ -61,92 +61,92 @@ end
 
 function ISMowingCursor:isRunningAction()
     local actionQueue = ISTimedActionQueue.getTimedActionQueue(self.character);
-    return actionQueue and actionQueue.queue and actionQueue.queue[1]
+    return actionQueue and actionQueue.queue and actionQueue.queue[1];
 end
 
 function ISMowingCursor:getTopLeftOfSquares(x, y, z)
 	if self.character:getJoypadBind() ~= -1 then
-		local cx,cy = math.floor(self.character:getX()), math.floor(self.character:getY())
+		local cx,cy = math.floor(self.character:getX()), math.floor(self.character:getY());
 		if self.character:isOnFire() then
-			return cx,cy,z
+			return cx,cy,z;
 		end
-		local dir = self.character:getDir()
-		if     dir == IsoDirections.N  then   x,y = cx,   cy-2
-		elseif dir == IsoDirections.NE then   x,y = cx+1, cy-2
-		elseif dir == IsoDirections.E  then   x,y = cx+1, cy
-		elseif dir == IsoDirections.SE then   x,y = cx+1, cy+1
-		elseif dir == IsoDirections.S  then   x,y = cx,   cy+1
-		elseif dir == IsoDirections.SW then   x,y = cx-2, cy+1
-		elseif dir == IsoDirections.W  then   x,y = cx-2, cy
-		elseif dir == IsoDirections.NW then   x,y = cx-2, cy-2
+		local dir = self.character:getDir();
+		if     dir == IsoDirections.N  then   x,y = cx,   cy-2;
+		elseif dir == IsoDirections.NE then   x,y = cx+1, cy-2;
+		elseif dir == IsoDirections.E  then   x,y = cx+1, cy;
+		elseif dir == IsoDirections.SE then   x,y = cx+1, cy+1;
+		elseif dir == IsoDirections.S  then   x,y = cx,   cy+1;
+		elseif dir == IsoDirections.SW then   x,y = cx-2, cy+1;
+		elseif dir == IsoDirections.W  then   x,y = cx-2, cy;
+		elseif dir == IsoDirections.NW then   x,y = cx-2, cy-2;
 		end
 	end
-	return x,y,z
+	return x,y,z;
 end
 
 function ISMowingCursor:render(x, y, z, square)
-	if self:isRunningAction() then return end
+	if self:isRunningAction() then return; end
 	
-	local bValid = self:isValidArea(x, y, z, true)
+	local bValid = self:isValidArea(x, y, z, true);
 	if bValid then
-		renderIsoRect(x + 1, y + 1, z, self.radius, ghc:getR(), ghc:getG(), ghc:getB(), 0.5, 1)
+		renderIsoRect(x + 1, y + 1, z, self.radius, ghc:getR(), ghc:getG(), ghc:getB(), 0.5, 1);
 	else
-		renderIsoRect(x + 1, y + 1, z, self.radius, bhc:getR(), bhc:getG(), bhc:getB(), 0.5, 1)
+		renderIsoRect(x + 1, y + 1, z, self.radius, bhc:getR(), bhc:getG(), bhc:getB(), 0.5, 1);
 	end
 		
 end
 
 function ISMowingCursor:onJoypadPressButton(joypadIndex, joypadData, button)
 	if button == Joypad.AButton or button == Joypad.BButton then
-		return ISBuildingObject.onJoypadPressButton(self, joypadIndex, joypadData, button)
+		return ISBuildingObject.onJoypadPressButton(self, joypadIndex, joypadData, button);
 	end
     if button == Joypad.YButton then
-        return self:rotateKey(getCore():getKey("Rotate building"))
+        return self:rotateKey(getCore():getKey("Rotate building"));
     end
 end
 
 function ISMowingCursor:getAPrompt()
-	return getText("ContextMenu_MowGrass")
+	return getText("ContextMenu_MowGrass");
 end
 
 function ISMowingCursor:getYPrompt()
-    return getText("ContextMenu_ChangeRadius")
+    return getText("ContextMenu_ChangeRadius");
 end
 
 function ISMowingCursor:getLBPrompt()
-	return nil
+	return nil;
 end
 
 function ISMowingCursor:getRBPrompt()
-	return nil
+	return nil;
 end
 
 function ISMowingCursor:getSquares(x, y, z)
-	local squares = {}
-	local square = getCell():getGridSquare(x, y, z)
-	table.insert(squares, square)
+	local squares = {};
+	local square = getCell():getGridSquare(x, y, z);
+	table.insert(squares, square);
 	for x2=x,x+self.radius-1 do
 		for y2=y,y+self.radius-1 do
-			local square = getCell():getGridSquare(x2, y2, z)
+			local square = getCell():getGridSquare(x2, y2, z);
 			if square then
-				table.insert(squares, square)
+				table.insert(squares, square);
 			end
 		end
 	end
-	return squares
+	return squares;
 end
 
 function ISMowingCursor:getClosestSquare(squares)
-	local closest = nil
-	local closestDist = 1000000
+	local closest = nil;
+	local closestDist = 1000000;
 	for _,square2 in ipairs(squares) do
-		local dist = IsoUtils.DistanceTo(self.character:getX(), self.character:getY(), square2:getX() + 0.5, square2:getY() + 0.5)
+		local dist = IsoUtils.DistanceTo(self.character:getX(), self.character:getY(), square2:getX() + 0.5, square2:getY() + 0.5);
 		if dist < closestDist then
-			closest = square2
-			closestDist = dist
+			closest = square2;
+			closestDist = dist;
 		end
 	end
-	return closest
+	return closest;
 end
 
 function ISMowingCursor:rotateKey(key)
@@ -159,23 +159,23 @@ function ISMowingCursor:rotateKey(key)
 end
 
 function ISMowingCursor:new(character, scythe)
-	local o = {}
-	setmetatable(o, self)
-	self.__index = self
-	o:init()
-	o.character = character
-	o.player = character:getPlayerNum()
-	o.skipBuildAction = true
-	o.noNeedHammer = true
-	o.renderFloorHelper = true
-	o.scythe = scythe
+	local o = {};
+	setmetatable(o, self);
+	self.__index = self;
+	o:init();
+	o.character = character;
+	o.player = character:getPlayerNum();
+	o.skipBuildAction = true;
+	o.noNeedHammer = true;
+	o.renderFloorHelper = true;
+	o.scythe = scythe;
 	o.radius = 3;
 	o.maxRadius = 3;
 	if scythe:getType() == "HandScythe" or scythe:hasTag(ItemTag.HAND_SCYTHE) then
 		o.radius = 1;
 		o.maxRadius = 1;
 	end
-	return o
+	return o;
 end
 
 Events.OnKeyPressed.Add(rotateKey);
